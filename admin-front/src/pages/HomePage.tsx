b9e9776ad91bd11a4df5c846d8f74cede49df369
ghtjr410 <ghtjr410@gmail.com>
2024-09-26 16:48:48 +0900

    admin-front - Jenkinsfile   - PowerShell 스크립트를 수정 - HomePage.tsx   - 더미코드 작성

--- admin-front/src/pages/HomePage.tsx
+++ admin-front/src/pages/HomePage.tsx
@@ -12,7 +12,7 @@ const HomePage = () => {
 // 
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-            <h1 className="text-2xl font-bold mb-4">이번엔될듯? 관리자 페이지 : Home</h1>
+            <h1 className="text-2xl font-bold mb-4">아닌듯 관리자 페이지 : Home</h1>
             <button 
                 onClick={goToOtherPage} 
                 className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700"
