db69def8972ce96b9b21cde680928580ae895d3f
ghtjr410 <ghtjr410@gmail.com>
2024-09-26 16:12:33 +0900

    Jenkins Test - 더미파일 작성

---
+++ admin-front/src/pages/Dummy.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const HomePage = () => {
+    const navigate = useNavigate();
+
+    const goToOtherPage = () => {
+        navigate("/other");
+    }
+
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+            <h1 className="text-2xl font-bold mb-4">ㅇㄴㅇㄴㅇㄴㅁㅇㄴㅁ 관리자 페이지 : Home</h1>
+            <button 
+                onClick={goToOtherPage} 
+                className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700"
+            >
+                Other 페이지로 이동
+            </button>
+        </div>
+    );
+}
+
+export default HomePage;
