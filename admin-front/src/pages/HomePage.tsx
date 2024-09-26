1d9989957d555f73ac1fcd4c4e112a6a89472323
ghtjr410 <ghtjr410@gmail.com>
2024-09-26 20:22:32 +0900

    Jenkins Test - 더미코드 작성

--- admin-front/src/pages/HomePage.tsx
+++ admin-front/src/pages/HomePage.tsx
@@ -12,7 +12,7 @@ const HomePage = () => {
 // 
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-            <h1 className="text-2xl font-bold mb-4">하루종일 할 수 있어8 관리자 페이지 : Home</h1>
+            <h1 className="text-2xl font-bold mb-4">하루종일 할 수 있어9 관리자 페이지 : Home</h1>
             <button 
                 onClick={goToOtherPage} 
                 className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700"
