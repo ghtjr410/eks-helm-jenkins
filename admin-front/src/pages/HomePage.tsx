92d1c6b96ac30cc2cf266b2e57f8a879ba73e24e
ghtjr410 <ghtjr410@gmail.com>
2024-09-27 09:45:55 +0900

    admin-front - Jenkinsfile   - Deploy to EKS Stage의 작업디렉토리를 dir('helm-charts')로 명시 - HomePage.tsx   - 더미코드 작성

--- admin-front/src/pages/HomePage.tsx
+++ admin-front/src/pages/HomePage.tsx
@@ -12,7 +12,7 @@ const HomePage = () => {
 // 
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-            <h1 className="text-2xl font-bold mb-4">0927-01 관리자 페이지 : Home</h1>
+            <h1 className="text-2xl font-bold mb-4">0927-02번째 테스트 관리자 페이지 : Home</h1>
             <button 
                 onClick={goToOtherPage} 
                 className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700"
